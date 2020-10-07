b0012b2944541eb836972dafa48c2fe443403531
ivan-piminov <ivan-piminov@yandex.ru>
2020-10-07 10:43:22 +0300

    Updates

---
+++ precache-manifest.9b8748d65f59b155c1e52324de2831d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b77ab12cad37d0d7d1544b5a38c72c9",
+    "url": "/KanbanDesk/index.html"
+  },
+  {
+    "revision": "ade4ff2af13988be2b40",
+    "url": "/KanbanDesk/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6afae084984f8f8a5fc2",
+    "url": "/KanbanDesk/static/js/2.fba11945.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KanbanDesk/static/js/2.fba11945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade4ff2af13988be2b40",
+    "url": "/KanbanDesk/static/js/main.86a5974d.chunk.js"
+  },
+  {
+    "revision": "1c0a45245fcafafc641a",
+    "url": "/KanbanDesk/static/js/runtime-main.eb34523b.js"
+  }
+]);
